beb99a77a29897c943accac3f0a277a78ddbf1c0
pulipatieswar77 <pulipatieswar77@gmail.com>
2019-06-26 12:09:48 +0530

    JAVA BASIC PROGS

---
+++ fundemantals/src/fundemantals/first.java
@@ -0,0 +1,12 @@
+package fundemantals;
+
+public class first {
+	public static void main(String args[])
+	{
+		int p,q,s=0;
+		p=Integer.parseInt(args[0]);
+		q=Integer.parseInt(args[0]);
+		s=p+q;
+		System.out.print("sum is:"+s);
+	}
+}
